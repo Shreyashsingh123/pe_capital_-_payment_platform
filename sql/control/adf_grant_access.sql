7fba22bddf6c10f37b232b317855c6edbca6c1cf
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-09-21 11:50:37 +0530

    [Shreyash] add. Granted access of sql and BLOB data reader

--- sql/control/adf_grant_access.sql
+++ sql/control/adf_grant_access.sql
@@ -5,3 +5,10 @@ CREATE USER [your-adf-resource-name] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_ddladmin ADD MEMBER [your-adf-resource-name];
 ALTER ROLE db_datawriter ADD MEMBER [your-adf-resource-name];
 ALTER ROLE db_datareader ADD MEMBER [your-adf-resource-name];
+
+--# permission for SQL ,Reader,Blob data reader
+CREATE USER [your-email-address] FROM EXTERNAL PROVIDER;
+
+ALTER ROLE db_ddladmin ADD MEMBER [your-email-address];
+ALTER ROLE db_datawriter ADD MEMBER [your-email-address];
+ALTER ROLE db_datareader ADD MEMBER [your-email-address];
